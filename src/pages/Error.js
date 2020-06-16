f4fb2f7125563035e8046b6d89cb9b4397f05c09
Rumeet Goradia <rumeet.goradia@gmail.com>
2020-06-15 20:20:28 -0400

    Change styling of 404 page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -14,16 +14,24 @@ export default function Error() {
 					<Row>
 						<Col
 							className="d-flex justify-content-center mt-4"
-							style={{ flexDirection: "column", textAlign: "center" }}
+							style={{ textAlign: "center" }}
 						>
 							<h1
-								style={{ fontSize: "18vmax", fontWeight: 900 }}
+								style={{ fontSize: "18vmax", fontWeight: 900, opacity: 0.1 }}
 								className="m-0"
 							>
 								404
 							</h1>
 							<h2
-								style={{ fontSize: "4vmax", fontWeight: 300 }}
+								style={{
+									fontSize: "4vmax",
+									fontWeight: 300,
+									position: "absolute",
+									top: "50%",
+									left: "50%",
+									transform: "translate(-50%, -50%)",
+									width: "100%",
+								}}
 								className="my-1"
 							>
 								There's nothing here.
