8f21bb9544ca1a33d0e89eda4c5ea8d7994358e8
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-10-27 13:47:32 -0400

    Add endpoint resolve

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -2,38 +2,42 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import { createTransport } from "nodemailer"
 
 function sendSubmissionEmail(req: NextApiRequest, res: NextApiResponse) {
-	if (req.body.url) {
-		res.status(400)
-		res.send("error")
-		return
-	}
-
-	const transporter = createTransport({
-		port: 465,
-		host: "smtp.gmail.com",
-		auth: {
-			user: process.env.SUBMISSION_EMAIL,
-			pass: process.env.SUBMISSION_PASSWORD,
-		},
-		secure: true,
-	})
-
-	const mailData = {
-		from: process.env.SUBMISSION_EMAIL,
-		to: process.env.CONTACT_EMAIL,
-		subject: `Advocate BLM: Submission from ${req.body.name}`,
-		text: req.body.argument,
-		html: `<pre>${JSON.stringify(req.body, null, 2)}</pre>`,
-	}
-
-	transporter.sendMail(mailData, function (err) {
-		if (err) {
-			res.status(500)
+	return new Promise((resolve) => {
+		if (req.body.url) {
+			res.status(400)
 			res.send("error")
-		} else {
-			res.status(200)
-			res.send("success")
+			resolve(null)
 		}
+
+		const transporter = createTransport({
+			port: 465,
+			host: "smtp.gmail.com",
+			auth: {
+				user: process.env.SUBMISSION_EMAIL,
+				pass: process.env.SUBMISSION_PASSWORD,
+			},
+			secure: true,
+		})
+
+		const mailData = {
+			from: process.env.SUBMISSION_EMAIL,
+			to: process.env.CONTACT_EMAIL,
+			subject: `Advocate BLM: Submission from ${req.body.name}`,
+			text: req.body.argument,
+			html: `<pre>${JSON.stringify(req.body, null, 2)}</pre>`,
+		}
+
+		transporter.sendMail(mailData, function (err) {
+			if (err) {
+				res.status(500)
+				res.send
+				resolve(null)
+			} else {
+				res.status(200)
+				res.send("success")
+				resolve(null)
+			}
+		})
 	})
 }
 
